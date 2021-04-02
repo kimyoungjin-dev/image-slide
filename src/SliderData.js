39a157e91f78c5aa4970a6529c6c217ad9e32788
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-03 03:32:15 +0900

    Create an image array called SliderData and export it.

---
+++ src/SliderData.js
@@ -0,0 +1,31 @@
+export const SliderData = [
+  {
+    id: 1,
+    image: require("./images/image-1.jpeg"),
+  },
+
+  {
+    id: 2,
+    image: require("./images/image-2.jpeg"),
+  },
+
+  {
+    id: 3,
+    image: require("./images/image-3.jpeg"),
+  },
+
+  {
+    id: 4,
+    image: require("./images/image-4.jpeg"),
+  },
+
+  {
+    id: 5,
+    image: require("./images/image-5.jpeg"),
+  },
+
+  {
+    id: 6,
+    image: require("./images/image-6.jpeg"),
+  },
+];
