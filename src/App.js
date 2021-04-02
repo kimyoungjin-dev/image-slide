2e6f09e33bf6030272ed8926c237974d3196c60f
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-03 03:32:40 +0900

    Do the css design and create prevButton, nextButton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { SliderData } from "./SliderData";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-function App() {
+const Box = styled.div`
+  position: relative;
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  width: 1000px;
+  height: 600px;
+`;
+
+const LeftArrow = styled(FaArrowLeft)`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  color: white;
+  border-radius: 50%;
+  left: 32px;
+`;
+
+const RightArrow = styled(FaArrowRight)`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  color: white;
+  border-radius: 50%;
+  top: 50%;
+  right: 32px;
+`;
+
+const App = () => {
+  const [current, setCurrent] = useState(0);
+
+  if (!Array.isArray(SliderData) || SliderData.length <= 0) {
+    return null;
+  }
+
+  const prevButton = () => {
+    setCurrent(current === 0 ? SliderData.length - 1 : current - 1);
+  };
+
+  const nextButton = () => {
+    setCurrent(current === SliderData.length - 1 ? 0 : current + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <LeftArrow onClick={prevButton} />
+      <RightArrow onClick={nextButton} />
+
+      {SliderData.map((v, i) => (
+        <Image src={v.image.default} />
+      ))}
+    </Box>
   );
-}
+};
 
 export default App;
